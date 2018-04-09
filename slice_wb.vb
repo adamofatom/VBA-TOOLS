13f52e3cbe66d066127ce3b9c95825f79c321344
adamofatom <yemengtao1986@hotmail.com>
2018-04-09 21:28:24 +0800

    done

--- slice_wb.vb
+++ slice_wb.vb
@@ -9,8 +9,7 @@ Sub CFGZB()
     Dim title As String
 
     Dim columnNum As Integer
- 
- 
+
     myRange = Application.InputBox(prompt:="请选择标题行：", Type:=8)
 
     myArray = WorksheetFunction.Transpose(myRange)
